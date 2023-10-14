a592fad5c7a5c4e2002018ed98a953a0f8d6f154
Johannes <joh@nnes.fi>
2023-10-14 13:49:46 +0300

    Added product datetime

--- receiptwallet/backend/schemas/product.py
+++ receiptwallet/backend/schemas/product.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class Product(BaseModel):
-    date: str
+    date: str = "today"
     product: str
     category: str
     price: float
+    
+    @field_validator('date')
+    def check_date(date):
+        if date == 'today':
+            return datetime.now().isoformat()
+        return date
